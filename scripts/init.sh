0a8b8415dc4b4b3f245a0c085e3fc55d1911c8ea
Ed Laur <mr.edlaur@gmail.com>
2019-03-19 13:26:19 +0200

    WIP

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,17 +2,15 @@
 
 # Connect to Kubernetes cluster
 gcloud container clusters get-credentials my-gke-cluster --zone us-east1-b --project awesome-devops
-/c/gcloud/google-cloud-sdk/bin/gsutil.cmd setmeta -R -h 'Cache-Control:public, max-age=0, no-transform' gs://awesome-devops-helm-charts/index.yaml
-# Install Tiller
-helm init --service-account tiller
-sleep 10
 
-# Initialize 
-helm repo add devops-charts https://awesome-devops-helm-charts.storage.googleapis.com
+# Install Tiller
+helm init --wait --service-account tiller
 
 # Install influxdb
 helm install stable/influxdb --wait --name influxdb --namespace monitoring --set persistence.enabled=true,persistence.size=10Gi
 kubectl exec -i -t --namespace monitoring $(kubectl get pods --namespace monitoring -l app=influxdb -o jsonpath='{.items[0].metadata.name}')  -- influx -execute 'create database response'
 
-helm install stable/traefik --name my-release --namespace kube-system 
 # kubectl port-forward --namespace monitoring $(kubectl get pods --namespace monitoring -l app=influxdb -o jsonpath='{ .items[0].metadata.name }') 8086:8086
+
+helm upgrade go-app-live --install --wait --namespace application --set image.tag=1.0 --set fullnameOverride=go-app-live ../helm/go-app
+helm upgrade go-app-canary --install --wait --namespace application --set image.tag=2.0 --set fullnameOverride=go-app-canary ../helm/go-app
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
 cd ../app
-docker build --build-arg app_version=3.0 -t edbighead/go-app:3.0 .
-docker push edbighead/go-app:3.0
+
+IMAGE_VERSION=""
+IMAGE_NAME="edbighead/go-app"
+
+if [ -z "$1" ] || [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+  echo "Example usage:"
+  echo "release.sh -v 1.0"
+  echo ""
+  echo "-v [mandatory]: docker image version"
+else
+  while getopts h:v: option; do
+    case "${option}" in
+    v) IMAGE_VERSION=${OPTARG} ;;
+    esac
+  done
+
+  if [ "$IMAGE_VERSION" = "" ]; then
+    echo "Version should be specified. Please see --help"
+    exit -1
+  fi
+
+  IMAGE="$IMAGE_NAME:$IMAGE_VERSION"
+  echo "building $IMAGE"
+
+  docker build --build-arg app_version=$IMAGE_VERSION -t $IMAGE .
+  docker push $IMAGE
+
+fi
